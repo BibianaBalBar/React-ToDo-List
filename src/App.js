7634d2e32dfd6f76aae84fcbfec68101b43cc27d
BibianaBalBar <balbuenabibi@gmail.com>
2020-09-26 00:30:51 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
-import Table from './Table';
-import Form from './Form';
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
   state = {
     listTodos: [],
-  }
+  };
 
   removeItem = (index) => {
-    const {listTodos} = this.state
-  
+    const { listTodos } = this.state;
+
     this.setState({
       listTodos: listTodos.filter((listTodo, i) => {
-        return i !== index
+        return i !== index;
       }),
-    })
-  }  
+    });
+  };
 
   handleSubmit = (listTodo) => {
-    this.setState({listTodos: [...this.state.listTodos, listTodo]})
-  }
+    this.setState({ listTodos: [...this.state.listTodos, listTodo] });
+  };
 
   render() {
-    const { listTodos } = this.state
+    const { listTodos } = this.state;
 
     return (
-      <div className="container">
+      <div className='container'>
         <h1>React To-Do List</h1>
-        <p>List here your To-Dos:</p>
+        <h5>List here your To-Dos:</h5>
         <Table listTodosData={listTodos} removeItem={this.removeItem} />
         <h3>Add New</h3>
         <Form handleSubmit={this.handleSubmit} />
       </div>
-    )
+    );
   }
 }
 
-  export default App
+export default App;
